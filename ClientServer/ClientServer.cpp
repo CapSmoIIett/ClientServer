40464d29973aba87bd1cacf8c60b469541af3aa0
MyWindows10 <CapSmoIIett>
2022-09-09 22:47:21 +0300

    Create basement of future app.

---
+++ ClientServer/ClientServer.cpp
@@ -0,0 +1,12 @@
+﻿// ClientServer.cpp: определяет точку входа для приложения.
+//
+
+#include "ClientServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
